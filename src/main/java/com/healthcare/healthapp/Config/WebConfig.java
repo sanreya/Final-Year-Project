eab11179b7d3abc705d030cde520631679dd7b59
Sanjana N Athreya <sanjanaathreyan@gmail.com>
2025-05-04 00:29:19 +0530

    deployed db

--- src/main/java/com/healthcare/healthapp/Config/WebConfig.java
+++ src/main/java/com/healthcare/healthapp/Config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:8080", "https://final-year-project-production-f1cb.up.railway.app/")
+                .allowedOrigins("http://localhost:8080", "https://final-year-project-production-f1cb.up.railway.app")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*");
     }
