ec1fb8a9576d5e4c6ce0101fd478c463f01eaf05
Sanjana N Athreya <sanjanaathreyan@gmail.com>
2025-05-05 09:36:34 +0530

    added diet recommendations

--- src/main/java/com/healthcare/healthapp/Controller/ProfileController.java
+++ src/main/java/com/healthcare/healthapp/Controller/ProfileController.java
@@ -1,13 +1,19 @@
 package com.healthcare.healthapp.Controller;
 
+import com.healthcare.healthapp.Entity.User;
+import com.healthcare.healthapp.Service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class ProfileController {
 
+    @Autowired
+    private UserService userService;
+
     /*@GetMapping
-    public <> profile(){
-        return "profile";
+    public User profile(){
+        return userService.fetchUser();
     }*/
 }
